3690410db35118a2a629c466ae0ebf63d511a889
Yusuf Avci <yusuf.avci@baogrenci.com>
2019-09-29 14:10:20 +0300

    Admin Paneli Login İşlemi Yapıldı
    (cherry picked from commit 3b16e4ed7028978a1abb10f59621d35b1533bd9a)

---
+++ NTierProject.WebUI/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using NTierProject.MODEL.Context;
+using NTierProject.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NTierProject.WebUI.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        ProjectContext db = new ProjectContext();
+
+        [HttpPost]
+        public ActionResult Login(AppUser kullanici)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ProjectContext context = new ProjectContext())
+                {
+                    var user = context.Users.FirstOrDefault(x =>x.UserName ==kullanici.UserName && x.Password == kullanici.Password);
+
+         
+                       
+                        return RedirectToAction("Home", "Index");
+                   
+
+                }
+            }
+
+            return View();
+        }
+    }
+}
